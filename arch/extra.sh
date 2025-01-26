51e2775eb431bd03fe7f5b92b991402730d951b7
Chris Lowles <lowleschris@gmail.com>
2025-01-27 06:43:02 +1100

    Update extra.sh

--- arch/extra.sh
+++ arch/extra.sh
@@ -10,7 +10,21 @@ sed -i 's/^#unix_sock_ro_perms = \"0777\"/unix_sock_ro_perms = \"0777\"/' /etc/l
 sed -i 's/^#unix_sock_rw_perms = \"0770\"/unix_sock_rw_perms = \"0770\"/' /etc/libvirt/libvirtd.conf
 usermod -aG libvirt $USERNAME
 sudo systemctl enable libvirtd
-#newgrp libvirt
+newgrp libvirt
+
+# locales for australia, iso codes too
+# en_AU.UTF-8
+# en_AU ISO-8859-1
+
+# install custom bashrc commands, shortcuts to common commands, push this into config somehow
+curl -Sk https://raw.githubusercontent.com/chrislowles/nix/main/arch/.bashrc >> ~/.bashrc
+source ~/.bashrc
+
+# read command needed for lsp/zam shortcut flush, if we're to install them
+#echo '[Desktop Entry]
+#Hidden=true' > /tmp/1
+#find /usr -name "*lsp_plug*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
+#find /usr -name "*zam*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 
 echo "adding direct flathub remote"
 sleep 1
@@ -20,8 +34,6 @@ echo "generic curl install for distrobox"
 sleep 1
 curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
 
-echo "flatpak permissions and cleaner, distrobox gui and other preferred flatpaks"
+# echo "flatpak permissions and cleaner, distrobox gui and other preferred flatpaks"
 sleep 1
-flatpak install flathub \
-com.github.tchx84.Flatseal io.github.giantpinkrobots.flatsweep \
-io.github.dvlv.boxbuddyrs com.feaneron.Boatswain org.kde.kdenlive io.freetubeapp.FreeTube com.discordapp.Discord org.qbittorrent.qBittorrent io.github.celluloid_player.Celluloid org.audacityteam.Audacity org.kde.krita io.github.shiftey.Desktop org.libreoffice.LibreOffice io.github.pwr_solaar.solaar org.upscayl.Upscayl com.spotify.Client io.github.peazip.PeaZip com.obsproject.Studio us.zoom.Zoom me.timschneeberger.jdsp4linux com.visualstudio.code com.valvesoftware.Steam com.steamgriddb.steam-rom-manager com.mojang.Minecraft uk.co.powdertoy.tpt io.itch.itch io.github.limo_app.limo net.rpcs3.RPCS3 org.DolphinEmu.dolphin-emu org.ppsspp.PPSSPP net.pcsx2.PCSX2 org.flycast.Flycast org.musicbrainz.Picard org.blender.Blender org.gnome.meld
+# flatpak install flathub \
